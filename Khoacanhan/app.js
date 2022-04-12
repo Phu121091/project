bd6bcb0ffbc9abd27c17c8a1c40f4f854b796ee0
Phu121091 <100426997+Phu121091@users.noreply.github.com>
2022-04-13 04:45:18 +0700

    Add files via upload

---
+++ Khoacanhan/app.js
@@ -0,0 +1,28 @@
+// const dkkh = document.querySelector(".dkkh");
+
+// // dkkh.addEventListener("click", (m) => {
+// // 	m.preventDefault();
+// // });
+
+const menubtn = document.querySelector(".menucol");
+const headermenu = document.querySelector(".header-center");
+
+menubtn.addEventListener("click", () => {
+  headermenu.classList.toggle("options-active");
+});
+
+const loging = document.querySelector("#login");
+const logou = document.querySelector(".logout");
+console.log(loging);
+const cuser = JSON.parse(localStorage.getItem("currentUser"));
+console.log(cuser);
+if (cuser!==``){
+loging.innerHTML=`${cuser.username}`;
+loging.setAttribute("href","/project/InforUser/InforUser.html");
+logou.classList.add("logout-active");}
+
+const logout = document.querySelector(".logoutbtn");
+logout.onclick = () =>{
+  localStorage.removeItem(`currentUser`);
+  location.reload();
+}
