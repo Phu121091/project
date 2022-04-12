73b9b6c811b0b88117677a9398ed58d7e78464ac
Phu121091 <100426997+Phu121091@users.noreply.github.com>
2022-04-13 04:43:30 +0700

    Add files via upload

--- Gioithieu/Gioithieu.js
+++ Gioithieu/Gioithieu.js
@@ -43,4 +43,25 @@ mapD.onclick = () =>{
     mapD.classList.add("mapnow");
 }
 
+const menubtn = document.querySelector(".menucol");
+const headermenu = document.querySelector(".header-center");
 
+menubtn.addEventListener("click", () => {
+  headermenu.classList.toggle("options-active");
+});
+
+const loging = document.querySelector("#login");
+const logou = document.querySelector(".logout");
+console.log(loging);
+const cuser = JSON.parse(localStorage.getItem("currentUser"));
+console.log(cuser);
+if (cuser!==``){
+loging.innerHTML=`${cuser.username}`;
+loging.setAttribute("href","/project/InforUser/InforUser.html");
+logou.classList.add("logout-active");}
+
+const logout = document.querySelector(".logoutbtn");
+logout.onclick = () =>{
+  localStorage.removeItem(`currentUser`);
+  location.reload();
+}
