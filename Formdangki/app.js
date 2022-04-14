d58d82c2189a67e4b6b1a9713c2a8f11c2185ba2
Phu121091 <100426997+Phu121091@users.noreply.github.com>
2022-04-15 00:19:13 +0700

    Update app.js

--- Formdangki/app.js
+++ Formdangki/app.js
@@ -5,39 +5,49 @@ const inputAddress = document.querySelector(".input-address");
 const inputEmail = document.querySelector(".input-email");
 const inputPhone = document.querySelector(".input-phone");
 const khoahoc =document.querySelector(".khoahoc");
+
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	alert("Đăng kí khóa học thành công");
-	// inputName.value = "";
-	// inputDate.value = "";
-	// inputAddress.value = "";
-	// inputEmail.value = "";
-	// inputPhone.value = "";
-	const cuser = JSON.parse(localStorage.getItem("currentUser"));
-if (cuser!==``){
+	const cruser =JSON.parse(localStorage.getItem("currentUser"));
+    console.log(cruser);
+	const users = localStorage.getItem("users")
+		? JSON.parse(localStorage.getItem("users"))
+		: [];
+    const cuser ={};
 	cuser.realname = `${inputName.value}`;
 	cuser.birthday = `${inputDate.value}`;
 	cuser.address = `${inputAddress.value}`;
 	cuser.email = `${inputEmail.value}`;
 	cuser.phone = `${inputPhone.value}`;
 	cuser.khoahoc = `${khoahoc.value}`;
-	localStorage.setItem("currentUser", JSON.stringify(cuser));
-	const users = localStorage.getItem("users")
-		? JSON.parse(localStorage.getItem("users"))
-		: [];
-		for(i=0;i<users.length;i++){
-			if(users[i].username == cuser.username){		
-		// users[i].password= cuser.password;
-		users[i].realname=cuser.realname;
-		users[i].birthday=cuser.birthday;
-		users[i].address=cuser.address;
-		users[i].email=cuser.email;
-		users[i].phone=cuser.phone;
-		users[i].khoahoc=cuser.khoahoc;
+	console.log(cuser);
+	if (cruser!==null){
+	const newcurrentuser = {...cruser,...cuser,};
+	localStorage.setItem("currentUser", JSON.stringify(newcurrentuser));
+	for(i=0;i<users.length;i++){
+			
+		if(users[i].username == cruser.username){
+	users[i].realname=cuser.realname;
+	users[i].birthday=cuser.birthday;
+	users[i].address=cuser.address;
+	users[i].email=cuser.email;
+	users[i].phone=cuser.phone;
+	users[i].khoahoc=cuser.khoahoc;
+};
+};
+}		
 
-		
-	};}
-	localStorage.setItem("users", JSON.stringify(users));
+else{
+	const userdangki = localStorage.getItem("khachdangki")
+		? JSON.parse(localStorage.getItem("khachdangki"))
+		: [];
+	userdangki.push(cuser);
+	localStorage.setItem("khachdangki", JSON.stringify(userdangki));
+
 }
+	localStorage.setItem("users", JSON.stringify(users));
+
 	window.location.href = "/project/Home/Home.html";
 });
