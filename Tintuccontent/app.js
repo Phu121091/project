e876ae6e901e50ec57d930c4717945b0ba32bf7b
Phu121091 <100426997+Phu121091@users.noreply.github.com>
2022-04-15 19:14:11 +0700

    Update app.js

--- Tintuccontent/app.js
+++ Tintuccontent/app.js
@@ -1,104 +1,104 @@
 const bangtin = [
-    {
-      id: 0,
-      tittle: `Golfer 14 tuổi vô địch Tiền Phong Golf Championship 2021`,
-      content: `<a href="/project/Tintuccontent/Tintuccontent0.html" class="linktintuc">
-      <div class="imgdiv"><img src="https://th.bing.com/th/id/OIP.o6HCSZtE1EkRJzCxqUkVsgHaFj?pid=ImgDet&rs=1" alt="" class="imgtt"></div> 
-       <h5 class="tittle">Golfer 14 tuổi vô địch Tiền Phong Golf Championship 2021</h5>
-   </a>`,
-    },
-    {
-      id: 1,
-      tittle: `Cặp kè đại gia U60, Hiền Hồ tổn thất nặng nề: Sụp đổ hình tượng, tẩy
-      chay, huỷ show`,
-      content: `<a href="/project/Tintuccontent/Tintuccontent1.html" class="linktintuc">
-      <div class="imgdiv"><img src="https://ttol.vietnamnetjsc.vn/images/2022/03/21/20/27/hien-ho-choi-golf-5.jpg" alt="" class="imgtt"></div>
-      <h5 class="tittle">Cặp kè đại gia U60, Hiền Hồ tổn thất nặng nề: Sụp đổ hình tượng, tẩy
-          chay, huỷ show</h5>
+  {
+    id: 0,
+    tittle: `Golfer 14 tuổi vô địch Tiền Phong Golf Championship 2021`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent0.html" class="linktintuc">
+    <div class="imgdiv"><img src="https://th.bing.com/th/id/OIP.o6HCSZtE1EkRJzCxqUkVsgHaFj?pid=ImgDet&rs=1" alt="" class="imgtt"></div> 
+     <h5 class="tittle">Golfer 14 tuổi vô địch Tiền Phong Golf Championship 2021</h5>
+ </a>`,
+  },
+  {
+    id: 1,
+    tittle: `Cặp kè đại gia U60, Hiền Hồ tổn thất nặng nề: Sụp đổ hình tượng, tẩy
+    chay, huỷ show`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent1.html" class="linktintuc">
+    <div class="imgdiv"><img src="https://ttol.vietnamnetjsc.vn/images/2022/03/21/20/27/hien-ho-choi-golf-5.jpg" alt="" class="imgtt"></div>
+    <h5 class="tittle">Cặp kè đại gia U60, Hiền Hồ tổn thất nặng nề: Sụp đổ hình tượng, tẩy
+        chay, huỷ show</h5>
+</a>`,
+  },
+  {
+    id: 2,
+    tittle: `Xu hướng nữ giới chơi golf: “Săn” đại gia hay tìm niềm kiêu hãnh trong những cú swing?`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent2.html" class="linktintuc">
+    <div class="imgdiv"><img src="https://th.bing.com/th/id/R.f9b892e2b1032bfaa23cf385c4d50269?rik=JaSLybhaPJtceQ&pid=ImgRaw&r=0" alt="" class="imgtt"></div>
+    <h5 class="tittle">Xu hướng nữ giới chơi golf: “Săn” đại gia hay tìm niềm kiêu hãnh trong những cú swing?</h5>
+</a>`,
+  },
+  {
+    id: 3,
+    tittle: `Bryson DeChambeau golf hàng đầu thế giới từng chơi golf rất tệ`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent3.html" class="linktintuc">
+      <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2021/12/hinh-anh-golf-hang-dau-the-gioi-1.jpg" alt="" class="imgtt"></div>
+      <h5 class="tittle">Bryson DeChambeau golf hàng đầu thế giới từng chơi golf rất tệ</h5>
   </a>`,
-    },
+    area:0,
+  },
     {
-      id: 2,
-      tittle: `Xu hướng nữ giới chơi golf: “Săn” đại gia hay tìm niềm kiêu hãnh trong những cú swing?`,
-      content: `<a href="/PJ/new2.html" class="linktintuc">
-      <div class="imgdiv"><img src="https://th.bing.com/th/id/R.f9b892e2b1032bfaa23cf385c4d50269?rik=JaSLybhaPJtceQ&pid=ImgRaw&r=0" alt="" class="imgtt"></div>
-      <h5 class="tittle">Xu hướng nữ giới chơi golf: “Săn” đại gia hay tìm niềm kiêu hãnh trong những cú swing?</h5>
+    id: 4,
+    tittle: `Ai là người giữ kỷ lục Hole-in-one ở Việt Nam?`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent4.html" class="linktintuc">
+      <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2018/05/nguoi-giu-ky-luc-hole-in-one-viet-nam.jpg" alt="" class="imgtt"></div>
+      <h5 class="tittle">Ai là người giữ kỷ lục Hole-in-one ở Việt Nam?</h5>
   </a>`,
-    },
-    {
-      id: 3,
-      tittle: `Bryson DeChambeau golf hàng đầu thế giới từng chơi golf rất tệ`,
-      content: `<a href="/PJ/new3.html" class="linktintuc">
-        <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2021/12/hinh-anh-golf-hang-dau-the-gioi-1.jpg" alt="" class="imgtt"></div>
-        <h5 class="tittle">Bryson DeChambeau golf hàng đầu thế giới từng chơi golf rất tệ</h5>
-    </a>`,
-      area:0,
-    },
-      {
-      id: 4,
-      tittle: `Ai là người giữ kỷ lục Hole-in-one ở Việt Nam?`,
-      content: `<a href="/PJ/new4.html" class="linktintuc">
-        <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2018/05/nguoi-giu-ky-luc-hole-in-one-viet-nam.jpg" alt="" class="imgtt"></div>
-        <h5 class="tittle">Ai là người giữ kỷ lục Hole-in-one ở Việt Nam?</h5>
-    </a>`,
-    },
-    {
-      id: 5,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
+  },
+  {
+    id: 5,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
+      <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
+      <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
+  </a>`,
+  },
+  {
+    id: 6,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
         <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
         <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
     </a>`,
-    },
-    {
-      id: 6,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
-          <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
-          <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
-      </a>`,
-    },
-    {
-      id: 7,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
-              <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
-              <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
-          </a>`,
-    },
-    {
-      id: 8,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
-                  <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
-                  <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
-              </a>`,
-    },
-    {
-      id: 9,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
-                      <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
-                      <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
-                  </a>`,
-    },
-    {
-      id: 10,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
-                          <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
-                          <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
-                      </a>`,
-    },
-    {
-      id: 11,
-      tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
-      content: `<a href="/PJ/new5.html" class="linktintuc">
-                              <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
-                              <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
-                          </a>`,
-    },
-  ];
+  },
+  {
+    id: 7,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
+            <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
+            <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
+        </a>`,
+  },
+  {
+    id: 8,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
+                <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
+                <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
+            </a>`,
+  },
+  {
+    id: 9,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
+                    <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
+                    <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
+                </a>`,
+  },
+  {
+    id: 10,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
+                        <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
+                        <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
+                    </a>`,
+  },
+  {
+    id: 11,
+    tittle: `Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt`,
+    content: `<a href="/project/Tintuccontent/Tintuccontent5.html" class="linktintuc">
+                            <div class="imgdiv"><img src="https://golftimes.vn/wp-content/uploads/2022/03/hinh-anh-hien-ho-choi-golf-1.jpg" alt="" class="imgtt"></div>
+                            <h5 class="tittle">Điểm Danh Top 5 Tay Golf Nữ HOT Nhất Trong Showbiz Việt</h5>
+                        </a>`,
+  },
+];
   
   //hàm tìm kiếm
   const searchvalue = document.querySelector(".searchinput");
